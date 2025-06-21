6aa75d560c28548c30ae627b2e10823573434e14
w1ldg00se <wildg00se@protonmail.com>
2025-06-21 16:59:27 +0200

    make progress bar smoother because parts where nothing needs modd updates are very fast and then the progress bar doesn't update very often

--- plexPlaylistDedup.py
+++ plexPlaylistDedup.py
@@ -159,7 +159,7 @@ def main():
 
         print(f"Applying mood '{moodName}' to duplicates...")
         # Note: applying moods in parallel didn't improve speed, this just takes some time
-        with tqdm(total=len(tracks)) as pbar:
+        with tqdm(total=len(tracks), maxinterval=1) as pbar:
             for track in uniqueTracks:
                 if getattr(track, "hasMood", False): # only delete mood from tracks that have the mood set
                     mood_del(track, moodName) # delete mood, in case tracks have it set so they are not filtered any more
